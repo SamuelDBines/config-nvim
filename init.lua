93b86ab44430dd6a515466f75469f213dca95fea
Samuel Bines <samuelBines@gmail.com>
2023-11-01 12:08:59 +0000

    Added nerdtree for better visual

--- init.lua
+++ init.lua
@@ -68,6 +68,7 @@ require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
   -- Git related plugins
+  'preservim/nerdtree',
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
   'github/copilot.vim',
@@ -268,7 +269,8 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
+vim.keymap.set('n', '<leader>n', '<cmd>NERDTreeFocus<CR>', { noremap=true, silent=true })
+vim.keymap.set('n', '<leader>m', '<cmd>NERDTreeToggle<CR>', { noremap=true, silent=true })
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
